99dc5f5f689920427b354bc9cfc0754ab37635d0
hmdyt <konnyaku108@gmail.com>
2022-03-06 15:06:16 +0900

    controller

---
+++ controller_20170425/objs/dep/hist.o.d
@@ -0,0 +1,132 @@
+objs/hist.o: hist.cc /usr/local/include/root/TFile.h \
+ /usr/local/include/root/TDirectoryFile.h \
+ /usr/local/include/root/TDirectory.h /usr/local/include/root/TNamed.h \
+ /usr/local/include/root/TObject.h /usr/local/include/root/Rtypes.h \
+ /usr/local/include/root/RtypesCore.h /usr/local/include/root/RConfig.h \
+ /usr/local/include/root/RVersion.h /usr/local/include/root/DllImport.h \
+ /usr/local/include/root/Rtypeinfo.h /usr/local/include/root/snprintf.h \
+ /usr/local/include/root/strlcpy.h \
+ /usr/local/include/root/TGenericClassInfo.h \
+ /usr/local/include/root/TSchemaHelper.h \
+ /usr/local/include/root/TStorage.h \
+ /usr/local/include/root/TVersionCheck.h \
+ /usr/local/include/root/Riosfwd.h /usr/local/include/root/TBuffer.h \
+ /usr/local/include/root/TString.h /usr/local/include/root/TMathBase.h \
+ /usr/local/include/root/RStringView.h \
+ /usr/local/include/root/RConfigure.h \
+ /usr/local/include/root/RWrap_libcpp_string_view.h \
+ /usr/local/include/root/libcpp_string_view.h \
+ /usr/local/include/root/TList.h /usr/local/include/root/TSeqCollection.h \
+ /usr/local/include/root/TCollection.h \
+ /usr/local/include/root/TIterator.h /usr/local/include/root/TDatime.h \
+ /usr/local/include/root/TUUID.h /usr/local/include/root/TMap.h \
+ /usr/local/include/root/THashTable.h /usr/local/include/root/TUrl.h \
+ /usr/local/include/root/TH1.h /usr/local/include/root/TAxis.h \
+ /usr/local/include/root/TAttAxis.h /usr/local/include/root/TArrayD.h \
+ /usr/local/include/root/TArray.h /usr/local/include/root/TAttLine.h \
+ /usr/local/include/root/TAttFill.h /usr/local/include/root/TAttMarker.h \
+ /usr/local/include/root/TArrayC.h /usr/local/include/root/TArrayS.h \
+ /usr/local/include/root/TArrayI.h /usr/local/include/root/TArrayF.h \
+ /usr/local/include/root/Foption.h /usr/local/include/root/TVectorFfwd.h \
+ /usr/local/include/root/TVectorDfwd.h \
+ /usr/local/include/root/TFitResultPtr.h
+
+/usr/local/include/root/TFile.h:
+
+/usr/local/include/root/TDirectoryFile.h:
+
+/usr/local/include/root/TDirectory.h:
+
+/usr/local/include/root/TNamed.h:
+
+/usr/local/include/root/TObject.h:
+
+/usr/local/include/root/Rtypes.h:
+
+/usr/local/include/root/RtypesCore.h:
+
+/usr/local/include/root/RConfig.h:
+
+/usr/local/include/root/RVersion.h:
+
+/usr/local/include/root/DllImport.h:
+
+/usr/local/include/root/Rtypeinfo.h:
+
+/usr/local/include/root/snprintf.h:
+
+/usr/local/include/root/strlcpy.h:
+
+/usr/local/include/root/TGenericClassInfo.h:
+
+/usr/local/include/root/TSchemaHelper.h:
+
+/usr/local/include/root/TStorage.h:
+
+/usr/local/include/root/TVersionCheck.h:
+
+/usr/local/include/root/Riosfwd.h:
+
+/usr/local/include/root/TBuffer.h:
+
+/usr/local/include/root/TString.h:
+
+/usr/local/include/root/TMathBase.h:
+
+/usr/local/include/root/RStringView.h:
+
+/usr/local/include/root/RConfigure.h:
+
+/usr/local/include/root/RWrap_libcpp_string_view.h:
+
+/usr/local/include/root/libcpp_string_view.h:
+
+/usr/local/include/root/TList.h:
+
+/usr/local/include/root/TSeqCollection.h:
+
+/usr/local/include/root/TCollection.h:
+
+/usr/local/include/root/TIterator.h:
+
+/usr/local/include/root/TDatime.h:
+
+/usr/local/include/root/TUUID.h:
+
+/usr/local/include/root/TMap.h:
+
+/usr/local/include/root/THashTable.h:
+
+/usr/local/include/root/TUrl.h:
+
+/usr/local/include/root/TH1.h:
+
+/usr/local/include/root/TAxis.h:
+
+/usr/local/include/root/TAttAxis.h:
+
+/usr/local/include/root/TArrayD.h:
+
+/usr/local/include/root/TArray.h:
+
+/usr/local/include/root/TAttLine.h:
+
+/usr/local/include/root/TAttFill.h:
+
+/usr/local/include/root/TAttMarker.h:
+
+/usr/local/include/root/TArrayC.h:
+
+/usr/local/include/root/TArrayS.h:
+
+/usr/local/include/root/TArrayI.h:
+
+/usr/local/include/root/TArrayF.h:
+
+/usr/local/include/root/Foption.h:
+
+/usr/local/include/root/TVectorFfwd.h:
+
+/usr/local/include/root/TVectorDfwd.h:
+
+/usr/local/include/root/TFitResultPtr.h:
